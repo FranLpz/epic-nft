db0e7d28cba76838047baac0806493c4077dfd4a
FranLpz <franciscoalopezd@gmail.com>
2021-12-12 13:08:51 -0500

    Generate NFT with random Background

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -16,15 +16,17 @@ contract MyEpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    // This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
-    // So, we make a baseSvg variable here that all our NFTs can use.
-    string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
-
-    // I create three arrays, each with their own theme of random words.
-    // Pick some random funny words, names of anime characters, foods you like, whatever! 
+    // We split the SVG at the part where it asks for the background color.
+    string svgPartOne = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
+    
+    string svgPartTwo = "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+   
     string[] firstWords = ["Super", "Amazing", "Awesome", "Digital", "Boost", "Killer"];
     string[] secondWords = ["Mustang", "Challenger", "Corolla", "Camaro", "Prius", "Model X"];
     string[] thirdWords = ["Fast", "Broken", "Fearless", "Spaceship", "Hover", "Busted"];
+
+    // Get fancy with it! Declare a bunch of colors.
+    string[] colors = ["red", "#08C2A8", "black", "yellow", "blue", "green"];
 
     //Add solidity event 
     event NewEpicNFTMinted(address sender, uint256 tokenId);
@@ -33,11 +35,8 @@ contract MyEpicNFT is ERC721URIStorage {
         console.log("This is my NFT contract. Ahoy!!!");
     }
 
-    // I create a function to randomly pick a word from each array.
     function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
-        // I seed the random generator. More on this in the lesson. 
         uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
-        // Squash the # between 0 and the length of the array to avoid going out of bounds.
         rand = rand % firstWords.length;
         return firstWords[rand];
     }
@@ -54,6 +53,13 @@ contract MyEpicNFT is ERC721URIStorage {
         return thirdWords[rand];
     }
 
+    // Same old stuff, pick a random color.
+    function pickRandomColor(uint256 tokenId) public view returns (string memory) {
+        uint256 rand = random(string(abi.encodePacked("COLOR", Strings.toString(tokenId))));
+        rand = rand % colors.length;
+        return colors[rand];
+    }
+
     function random(string memory input) internal pure returns (uint256) {
         return uint256(keccak256(abi.encodePacked(input)));
     }
@@ -66,7 +72,10 @@ contract MyEpicNFT is ERC721URIStorage {
         string memory third = pickRandomThirdWord(newItemId);
         string memory combinedWord = string(abi.encodePacked(first, second, third));
 
-        string memory finalSvg = string(abi.encodePacked(baseSvg, combinedWord, "</text></svg>"));
+        // Add the random color in.
+        string memory randomColor = pickRandomColor(newItemId);
+
+        string memory finalSvg = string(abi.encodePacked(svgPartOne,randomColor, svgPartTwo, combinedWord, "</text></svg>"));
 
         // Get all the JSON metadata in place and base64 encode it.
         string memory json = Base64.encode(
